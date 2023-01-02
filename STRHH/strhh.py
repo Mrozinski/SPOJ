5550174568df5c1e7326737a155d002887ec6b85
Paweł <pawel.mrozinski@iz.edu.pl>
2023-01-02 19:34:04 +0100

    AC

---
+++ STRHH/strhh.py
@@ -0,0 +1,10 @@
+# STRHH - Half of the half
+# https://www.spoj.com/problems/STRHH/
+# Paweł Mroziński
+# 02-01-2023
+
+n = int(input())
+for _ in range(n):
+    word = input()
+    l = len(word)
+    print(''.join([word[i] for i in range(int(l/2)) if i%2==0]))
